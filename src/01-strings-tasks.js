cb51c70c971f3d80ac7c52da9c8e19e1ecc71118
Rustam <tabarzin@gmail.com>
2023-05-21 00:28:32 +0300

    feat: solve getRectangleString

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Returns the result of concatenation of two strings.
  *
@@ -21,7 +20,6 @@
 function concatenateStrings(value1, value2) {
   return value1 + value2;
 }
-
 
 /**
  * Returns the length of given string.
@@ -68,7 +66,6 @@ function getStringFromTemplate(firstName, lastName) {
 function extractNameFromTemplate(value) {
   return value.slice(7, -1);
 }
-
 
 /**
  * Returns a first char of the given string.
@@ -130,7 +127,6 @@ function removeFirstOccurrences(str, value) {
   return str.replace(value, '');
 }
 
-
 /**
  * Remove the first and last angle brackets from tag string
  *
@@ -145,7 +141,6 @@ function removeFirstOccurrences(str, value) {
 function unbracketTag(str) {
   return str.slice(1, -1);
 }
-
 
 /**
  * Converts all characters of the specified string into the upper case
@@ -203,10 +198,13 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleString(width, height) {
+  const line = '─'.repeat(width - 2);
+  const firstPart = `┌${line}┐\n`;
+  const middlePart = `│${' '.repeat(width - 2)}│\n`.repeat(height - 2);
+  const lastPart = `└${line}┘\n`;
+  return `${firstPart}${middlePart}${lastPart}`;
 }
-
 
 /**
  * Encode specified string with ROT13 cipher
@@ -225,7 +223,9 @@ function getRectangleString(/* width, height */) {
  *
  */
 function encodeToRot13(str) {
-  const alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+  const alpha =
+    'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+
   return str.replace(/[a-z]/gi, (letter) => alpha[alpha.indexOf(letter) + 13]);
 }
 
@@ -245,7 +245,6 @@ function encodeToRot13(str) {
 function isString(value) {
   return value instanceof String || typeof value === 'string';
 }
-
 
 /**
  * Returns playid card id.
@@ -284,7 +283,6 @@ function getCardId(/* value */) {
   // }
   throw new Error('Not implemented');
 }
-
 
 module.exports = {
   concatenateStrings,
